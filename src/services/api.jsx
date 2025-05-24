dac07de133355213d9e1181252a71dbef37d3605
Feekkk <zxduke350@gmail.com>
2025-05-24 10:58:53 +0800

    setup groq api

---
+++ src/services/api.jsx
@@ -0,0 +1,8 @@
+import Groq from 'groq-js';
+
+export const groq = new Groq({
+    apiKey: import.meta.env.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,
+    URL: import.meta.env.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,
+    dangerouslyAllowAnyQuery: true,
+});
+
